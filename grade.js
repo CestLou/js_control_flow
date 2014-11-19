a6c02e6a6715044bc3bd57ee9bd942eaa24fc499
CestLou <alisonrebeccarobb@gmail.com>
2014-11-18 21:32:59 -0800

    Third homework assignment

---
+++ grade.js
@@ -0,0 +1,28 @@
+var testScore = 60;
+var result = "";
+
+if (testScore >= 90) {
+	result = "A"
+}
+else if (testScore >= 80) {
+	result = "B"
+}
+else if (testScore >= 70) {
+	result = "C"
+}
+else if (testScore >= 60) {
+	result = "D"
+}
+else {
+	result = "F"
+}
+
+console.log(result)
+
+
+
+// switch (testScore) {
+// 	case testScore >= 90git 
+// 	result= "A":
+// 	break;
+// }
