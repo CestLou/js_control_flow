64475fa8107c8d05ff395e8575744b097a9e024f
CestLou <alisonrebeccarobb@gmail.com>
2014-11-18 18:52:16 -0800

    First two homework assignments

---
+++ reverse.js
@@ -0,0 +1,7 @@
+var string="building";
+var reversedString="";
+for (var i = string.length - 1; i > -1; i--) {
+ // console.log(string[i]);
+ reversedString+= string[i]
+}
+ console.log(reversedString)
